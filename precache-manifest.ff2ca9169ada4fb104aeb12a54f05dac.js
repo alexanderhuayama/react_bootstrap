f489363626ad8790f5e7f53e7b0418f0b0db92dd
Alexander Huayama <alexander.hh.cc@gmail.com>
2019-01-09 04:46:32 -0500

    Initial commit

---
+++ precache-manifest.ff2ca9169ada4fb104aeb12a54f05dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fd6ee1ca2aa00824d83",
+    "url": "/react_bootstrap/static/js/runtime~main.8fd6ee1c.js"
+  },
+  {
+    "revision": "1bb489cebcee7ca904a5",
+    "url": "/react_bootstrap/static/js/main.1bb489ce.chunk.js"
+  },
+  {
+    "revision": "81c1c96b3e35beb57ecd",
+    "url": "/react_bootstrap/static/js/1.81c1c96b.chunk.js"
+  },
+  {
+    "revision": "1bb489cebcee7ca904a5",
+    "url": "/react_bootstrap/static/css/main.9eef7875.chunk.css"
+  },
+  {
+    "revision": "57ee791069bbf877bc2aee2386820de2",
+    "url": "/react_bootstrap/index.html"
+  }
+];
